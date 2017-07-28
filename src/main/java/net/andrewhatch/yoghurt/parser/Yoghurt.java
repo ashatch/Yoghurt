5c99b3373d72dca2bd26259fcd803425db0793b2
Andrew Hatch <ahatch@amplience.com>
2017-07-28 09:03:47 +0100

    check argv

--- src/main/java/net/andrewhatch/yoghurt/parser/Yoghurt.java
+++ src/main/java/net/andrewhatch/yoghurt/parser/Yoghurt.java
@@ -4,12 +4,16 @@ import com.google.inject.Guice;
 
 import net.andrewhatch.yoghurt.parser.interpreter.Parser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.inject.Inject;
 
 public class Yoghurt {
+  private static final Logger log = LoggerFactory.getLogger(Yoghurt.class);
 
   private final Parser parser;
   private final InputStream inputStream;
@@ -26,6 +30,10 @@ public class Yoghurt {
   }
 
   public static void main(String[] args) throws IOException {
+    if (args.length != 1) {
+      log.info("Must supply a file argument. Exiting.");
+      System.exit(1);
+    }
     Guice.createInjector(new YoghurtModule(args))
         .getInstance(Yoghurt.class)
         .run();
